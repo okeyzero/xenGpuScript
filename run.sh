54b5c0c83123a0b799043eba5f495453280dbb6d
okeyzero <48344256+okeyzero@users.noreply.github.com>
2023-09-30 02:01:22 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# 检查参数是否提供
-if [ $# -ne 1 ]; then
-  echo "Error: Please provide the account address."
-  echo "Usage: $0 <account_address>"
+# 检查参数是否为空
+if [ -z "$2" ]; then
+  echo "请提供账户地址作为参数"
   exit 1
 fi
 
-# 接收参数
-account_address=$1
-
 # 安装依赖
-sudo apt install ocl-icd-opencl-dev
-sudo apt-get install cmake make g++
+sudo apt install -y ocl-icd-opencl-dev
+sudo apt-get install -y cmake make g++
 
-# 克隆代码
+# 克隆代码并编译
 git clone https://github.com/shanhaicoder/XENGPUMiner.git
 cd XENGPUMiner
-
-# 编译
 chmod +x build.sh
-./build.sh
+if [ -n "$1" ]; then
+  ./build.sh -cuda_arch $1
+else
+  ./build.sh
+fi
 
-# 启动矿工和挖矿程序
-nohup ./xengpuminer -b 1024 &
+# 启动矿工程序
+nohup ./xengpuminer -b 1024 > miner.out 2>&1 &
+
+# 安装Python依赖并启动Python脚本
 pip3 install -U -r requirements.txt
-nohup python3 ./miner.py --gpu=true --account=$account_address > miner.out 2>&1 &
+nohup python3 ./miner.py --gpu=true --account=$2 > miner_python.out 2>&1 &
 
-# 查看 gpu计算日志 tail -f nohup.out
-# 查看 挖矿日志  tail -f miner.out
+echo "脚本执行完毕"
